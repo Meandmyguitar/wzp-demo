282982b9b3a36e611913421a93e2097fe4ecf3e7
王政鹏 <zhengpeng.wang@lanmaoly.com>
2020-07-09 23:21:46 +0800

    leetcode

---
+++ leetcode/src/main/java/com/demo/leetcode/sum/SumTest.java
@@ -0,0 +1,46 @@
+package com.demo.leetcode.sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SumTest {
+
+
+    /**
+     * 两数之和
+     * <p>
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     *
+     * <p>两遍哈希表</p>
+     * <p>一遍哈希表</p>
+     */
+    //    两遍哈希表
+    public int[] twoSum_2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    //    一遍哈希表
+    public int[] twoSum_1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
