c3d3f52bbce22d7d8a27f43143ddd1b268e0f39c
王政鹏 <zhengpeng.wang@lanmaoly.com>
2020-07-19 10:07:27 +0800

    juc

--- juc/src/main/java/com/demo/juc/conditionThread/CyclicBarrierDemo.java
+++ juc/src/main/java/com/demo/juc/conditionThread/CyclicBarrierDemo.java
@@ -1,11 +1,17 @@
 package com.demo.juc.conditionThread;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 
 public class CyclicBarrierDemo {
+
     public static void main(String[] args) {
-        cyclicBarrierTest();
+//        cyclicBarrierTest();
+        SummaryCyclicBarrierTest();
     }
 
     public static void cyclicBarrierTest() {
@@ -26,4 +32,31 @@ public class CyclicBarrierDemo {
             }, "" + i).start();
         }
     }
+
+
+    private static void SummaryCyclicBarrierTest() {
+        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> {
+            System.out.println("====召唤神龙=====" + concurrentHashMap);
+        });
+        for (int i = 1; i <= 7; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                concurrentHashMap.put("神龙" + finalI, getChar(finalI));
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+
+    private static String getChar(int i) {
+        return i + "";
+    }
+
+
 }
