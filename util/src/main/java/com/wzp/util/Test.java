9eb599de05f6ce500a3d61f838b54a16c78ef51c
王政鹏 <zhengpeng.wang@lanmaoly.com>
2020-07-26 19:41:34 +0800

    fix

---
+++ util/src/main/java/com/wzp/util/Test.java
@@ -0,0 +1,9 @@
+package com.wzp.util;
+
+public class Test {
+
+    public static void main(String[] args) {
+        new WzpDemo();
+        new WzpDemo();
+    }
+}
---
+++ util/src/main/java/com/wzp/util/WzpDemo.java
@@ -0,0 +1,18 @@
+package com.wzp.util;
+
+public class WzpDemo {
+
+    public WzpDemo() {
+        System.out.println("wzp-demo");
+    }
+
+    public static void staticMethod() {
+        System.out.println("static-method");
+    }
+
+    public void un_staticMethod() {
+        System.out.println("un-static-method");
+    }
+
+
+}
